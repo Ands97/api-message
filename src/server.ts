0cfaa94d9399a8851a27d5d3e5643f76dc07647c
Anderson Afonso <aafonso56@gmail.com>
2022-03-22 21:29:50 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,22 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
-import {mongoConnect} from './database/mongo'
 import cors from 'cors'
-import apiRoutes from './routes/api'
 
 
 
 dotenv.config();
-mongoConnect()
 const server = express();
 server.use(cors({}));
 
 server.use(express.static(path.join(__dirname, '../public')));
 server.use(express.json())
 server.use(express.urlencoded({extended: true}));
-server.use('/api', apiRoutes)
+
+
+const webhook = axios.get('http://')
+
+
 
 server.use('*', (req: Request, res: Response)=>{
     res.status(404).json({error: 'Endpoint não encontrado'})

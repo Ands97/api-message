312d66ebb20d4be3d2c34c224f9414f5d30dbdd9
Anderson Afonso <aafonso56@gmail.com>
2022-03-25 08:51:10 -0300

    Changes on server

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,29 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
-import cors from 'cors'
-
+import cors from 'cors';
+import axios from 'axios';
+import bodyParser from 'body-parser' 
 
 
 dotenv.config();
 const server = express();
 server.use(cors({}));
 
-server.use(express.static(path.join(__dirname, '../public')));
-server.use(express.json())
+
+server.use(bodyParser.json())
 server.use(express.urlencoded({extended: true}));
 
 
-const webhook = axios.get('http://')
-
-
-
-server.use('*', (req: Request, res: Response)=>{
-    res.status(404).json({error: 'Endpoint não encontrado'})
+server.post('/sendMessage', async (req: Request, res: Response) => {
+    const data = req.body
+    const ProductName = data.data.product.name
+    const name = data.data.producer.name;
+    const number = '83986538971'
+    const message = `Olá, ${name}! seu pedido do produto ${ProductName} foi feito com sucesso!`
+    console.log(message)
+    await axios.post('http://localhost:8000/send-message', {number, message})
+    res.status(200).end()
 })
 
 server.listen(process.env.PORT);
